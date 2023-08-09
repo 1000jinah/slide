2fe993c279fe13992e93943054012cccf52488ae
100jinah <winwalking1106@gmail.com>
2023-08-09 10:27:27 +0900

    230809_링크_default_설정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { CssBaseline } from "@mui/material";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Related from "screens/related";
 import Recommend from "screens/recommend";
 import Detail from "screens/detail";
 import Editor from "screens/editor";
+
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <CssBaseline />
         <Routes>
+          {/* Redirect from / to /recommend */}
+          <Route path="/" element={<Navigate to="/recommend" replace />} />
           <Route path="/related" element={<Related />} />
           <Route path="/recommend" element={<Recommend />} />
           <Route path="/detail" element={<Detail />} />
