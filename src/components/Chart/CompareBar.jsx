2b71aa7adb118d9b398ea0610be20e0fec06461f
100jinah <winwalking1106@gmail.com>
2023-08-09 11:03:25 +0900

    230809_모듈_제거

--- src/components/Chart/CompareBar.jsx
+++ src/components/Chart/CompareBar.jsx
@@ -3,41 +3,35 @@ import Chart from "chart.js/auto";
 
 const VerticalBarChart = ({ labels, data, backgroundColors }) => {
   const chartRef = useRef(null);
-  const chartInstanceRef = useRef(null); // Store the chart instance
+  const chartInstanceRef = useRef(null);
 
   useEffect(() => {
     const ctx = chartRef.current.getContext("2d");
 
-    // Destroy previous chart instance, if it exists
     if (chartInstanceRef.current) {
       chartInstanceRef.current.destroy();
     }
 
-    // Register datalabels plugin
+    const barWidth = 1.2 / data.length;
 
-    // Calculate the bar width based on the number of datasets
-    const barWidth = 1.2 / data.length; // Adjust the bar width percentage
-
-    // Create new chart instance
     chartInstanceRef.current = new Chart(ctx, {
       type: "bar",
       data: {
         labels: labels,
         datasets: data.map((dataset, index) => ({
           data: dataset,
-          backgroundColor: backgroundColors[index] || "#c8cacc", // Transparent background color
-          borderWidth: 0, // Remove the border around the chart area
-          barPercentage: barWidth, // Set the bar width dynamically
+          backgroundColor: backgroundColors[index] || "#c8cacc",
+          borderWidth: 0,
+          barPercentage: barWidth,
         })),
       },
       options: {
         scales: {
           y: {
             beginAtZero: true,
-            maxTicksLimit: 7, // Set max tick count to 7
-
+            maxTicksLimit: 7,
             ticks: {
-              callback: (value) => `${value}`, // Custom tick label format
+              callback: (value) => `${value}`,
             },
             grid: {
               color: "#c8cacc",
@@ -47,14 +41,7 @@ const VerticalBarChart = ({ labels, data, backgroundColors }) => {
         },
         plugins: {
           legend: {
-            display: false, // Hide the legend
-          },
-          datalabels: {
-            anchor: "end",
-            align: "end",
-            offset: 2,
-            color: "#636973", // Set the value color to black
-            formatter: (value) => value, // Display the value on top of the bar
+            display: false,
           },
         },
         layout: {
@@ -65,11 +52,10 @@ const VerticalBarChart = ({ labels, data, backgroundColors }) => {
             right: 0,
           },
         },
-        maintainAspectRatio: false, // Prevent chart resizing on hover
+        maintainAspectRatio: false,
       },
     });
 
-    // Clean up on component unmount
     return () => {
       if (chartInstanceRef.current) {
         chartInstanceRef.current.destroy();
